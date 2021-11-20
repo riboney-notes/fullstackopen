84700bdb1f27d4b11417bc68ae8fc86d1af40ab4
ribone <14286113+riboney@users.noreply.github.com>
2021-11-19 20:37:05 -0500

    Adds notes to part 1

---
+++ part1/intro/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// Renders the contents of the App.js component
+//    to a div element defined by the `public/index.html`
+//    file with an id value of 'root'
+ReactDOM.render(<App />,document.getElementById('root'))
